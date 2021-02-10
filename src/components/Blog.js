2604e24a05d6b16136a699a8a5ae00aa789f5a3f
diego <diegomotta18@gmail.com>
2021-02-10 16:29:01 -0300

    ADD: add component sidebar

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -5,9 +5,11 @@ import { Link } from "gatsby"
 const Blog = ({id,title,image,date,category,slug,desc}) => {
   return (<Link to={ `/blogs/${slug}`} key={id}>
       <article>
-        <Image fluid={image.childImageSharp.fluid} className="blog-img"/>
+        {image &&
+                <Image fluid={image.childImageSharp.fluid} className="blog-img"/>
+        }
         <div className="blog-card">
-          <h4>{title}</h4>
+          <h4>{title || "default title"}</h4>
           <p>{desc}</p>
           <div className="blog-footer">
             <p>{category}</p>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,7 +7,9 @@ const Layout = ({ children }) => {
   return (
     <>
     <Navbar></Navbar>
+    <Sidebar/>
       {children}
+      <Footer />
     </>
   )
 }
